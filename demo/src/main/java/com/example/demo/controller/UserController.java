56abe2f06bae50964809e8f4c4a1cf21a75ca68c
achraf abaab <achrafabaab>
2024-03-13 11:43:36 +0100

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/form")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submitForm(User user) {
+        userRepository.save(user);
+        return "redirect:/confirmation";
+    }
+}
